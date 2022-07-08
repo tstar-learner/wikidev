3bf2f2160a32482b4651eadb70843299ce16fbd1
Tong <731557539@qq.com>
2022-07-08 21:50:41 +0800

    2-3 项目初始化配置

---
+++ src/main/java/com/jiawa/wikidev/WikidevApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.wikidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikidevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikidevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiawa/wikidev/WikidevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.wikidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikidevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
