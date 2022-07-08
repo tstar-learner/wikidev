e6cf356866307764d0f7f7fddb314031153b8c74
Tong <731557539@qq.com>
2022-07-08 22:23:22 +0800

    2-5 修改启动，增加日志

--- src/main/java/com/jiawa/wikidev/WikidevApplication.java
+++ src/main/java/com/jiawa/wikidev/WikidevApplication.java
@@ -1,13 +1,21 @@
 package com.jiawa.wikidev;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikidevApplication {
 
+    private static final Logger LOG=  LoggerFactory.getLogger(WikidevApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikidevApplication.class, args);
+        SpringApplication app=new SpringApplication(WikidevApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
