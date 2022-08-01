b0792d925ab2f7b3c7537f92e798e0438f1f7eef
Tong <731557539@qq.com>
2022-08-01 16:33:34 +0800

    6-2 增加电子书管理页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/admin/admin-ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
   },
   {
     path: '/about',
