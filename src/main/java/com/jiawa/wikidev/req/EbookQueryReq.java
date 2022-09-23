b3fe136cb2496e088947cba76cfb7629ce9cde18
Tong <731557539@qq.com>
2022-09-23 20:43:18 +0800

    9-2 用户表设计并生成持久层代码

--- src/main/java/com/jiawa/wikidev/req/EbookQueryReq.java
+++ src/main/java/com/jiawa/wikidev/req/EbookQueryReq.java
@@ -7,6 +7,8 @@ public class EbookQueryReq extends PageReq {
 
     private String name;
 
+    private Long categoryId2;
+
     public Long getCategoryId2() {
         return categoryId2;
     }
@@ -15,7 +17,7 @@ public class EbookQueryReq extends PageReq {
         this.categoryId2 = categoryId2;
     }
 
-    private Long categoryId2;
+
 
     public Long getId() {
         return id;
