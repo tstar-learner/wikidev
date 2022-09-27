40bcdf6e61475ef235159076308c86892fb1d6a3
Tong <731557539@qq.com>
2022-09-27 15:52:39 +0800

    9-6 修改时不能改密码

--- src/main/java/com/jiawa/wikidev/controller/UserController.java
+++ src/main/java/com/jiawa/wikidev/controller/UserController.java
@@ -7,10 +7,12 @@ import com.jiawa.wikidev.resp.CommonResp;
 import com.jiawa.wikidev.resp.UserQueryResp;
 import com.jiawa.wikidev.resp.PageResp;
 import com.jiawa.wikidev.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -29,6 +31,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
